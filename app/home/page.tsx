0ecd8413e1ff5ffd59f8f545d46a082e94a26d30
Rohit Shukla <shuklarohit2105@gmail.com>
2024-03-15 14:17:07 +0530

    added more components

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,51 +1,28 @@
-"use client"
-
 import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Join from './join';
+import React from 'react';
+import Navbar from './navbar';
+// import {Join} from "@/app/home/join"
 
 const LandingPage = () => {
-    // State to store the current time
-    const [currentTime, setCurrentTime] = useState(new Date());
-
-    // Function to update the current time
-    const updateCurrentTime = () => {
-        setCurrentTime(new Date());
-    };
-
-    // Effect to update the time every minute
-    useEffect(() => {
-        const intervalId = setInterval(updateCurrentTime, 60000); // Update every minute
-
-        // Cleanup function to clear the interval on component unmount
-        return () => clearInterval(intervalId);
-    }, []);
-
-    // Function to get the day of the week
-    const getDayOfWeek = () => {
-        const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        return daysOfWeek[currentTime.getDay()];
-    };
+    
 
     return (
-        <div className="flex min-h-screen p-8">
+        <div className="flex min-h-screen px-8">
+            <Navbar/>
+            
             <div className="flex flex-col justify-center w-1/2 p-8">
                 <h1 className="text-5xl font-bold mb-2">Collaborate, Build, Enjoy!</h1>
                 <div className="mt-4">
                     <Button className="mr-4">Join a meet</Button>
                     <Button variant="secondary">Host a meet</Button>
                 </div>
+            {/* <Join /> */}
 
-                <div className="flex mt-4 text-slate-500 text-lg items-center">
-                    {currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                    <Separator orientation='vertical' className='mx-2' />
-                    {getDayOfWeek()}
-                </div>
+                
 
             </div>
         </div>
+            
     );
 };
 
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,88 +17,95 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 import { Separator } from "@/components/ui/separator"
-import { FaGoogle } from "react-icons/fa6";
+import { FaGoogle, FaGithub } from "react-icons/fa6";
 import React from 'react'
 
 const page = () => {
   return (
     <div className="flex just-center h-screen p-14">
-        <div className="w-full flex justify-center items-center">
-          <Tabs defaultValue="signin" className="w-[400px]">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="signin">Sign In</TabsTrigger>
-              <TabsTrigger value="signup">Sign Up</TabsTrigger>
-            </TabsList>
-            <TabsContent value="signin">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Sign In</CardTitle>
-                  <CardDescription>
-                    Enter your credentials to sign in. If you don't have an account, you can sign up.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="signInUsername">Username</Label>
-                    <Input id="signInUsername" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="signInPassword">Password</Label>
-                    <Input id="signInPassword" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter className="flex flex-col">
-                  <Button className="w-full">Sign In</Button>
-                  <div className="w-full flex justify-center mt-6 mb-4">
-                    <Separator className="w-1/3"/>
-                    <span className="text-[10px] text-slate-500 w-fit mx-4 -mt-1 uppercase">or continue with</span>
-                    <Separator className="w-1/3"/>
-                  </div>
-                  <Button variant="outline" className="w-12 h-12 rounded-full">
-                    <FaGoogle />
+      <div className="w-full flex justify-center items-center">
+        <Tabs defaultValue="signin" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signin">Sign In</TabsTrigger>
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signin">
+            <Card className="shadow-2xl shadow-slate-900 bg-slate-900 bg-opacity-10">
+              <CardHeader>
+                <CardTitle>Sign In</CardTitle>
+                <CardDescription>
+                  Enter your credentials to sign in. If you don't have an account, you can sign up.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="signInUsername">Username</Label>
+                  <Input id="signInUsername" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="signInPassword">Password</Label>
+                  <Input id="signInPassword" type="password" />
+                </div>
+              </CardContent>
+              <CardFooter className="flex flex-col">
+                <Button className="w-full">Sign In</Button>
+                <div className="w-full flex justify-center mt-6 mb-4">
+                  <Separator className="w-[30%]" />
+                  <span className="text-[10px] text-slate-500 w-fit mx-4 -mt-1 uppercase">or continue with</span>
+                  <Separator className="w-[30%]" />
+                </div>
+                <div className="flex justify-center space-x-4">
+                  <Button variant="outline" className="w-12 h-12 rounded-full p-0">
+                    <FaGoogle className="text-lg" />
                   </Button>
+                  <Button variant="outline" className="w-12 h-12 rounded-full p-0">
+                    <FaGithub className="text-xl" />
+                  </Button>
+                </div>
 
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="signup">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Sign Up</CardTitle>
-                  <CardDescription>
-                    Create a new account by providing the required information.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="signUpName">Name</Label>
-                    <Input id="signUpName" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="signUpUsername">Username</Label>
-                    <Input id="signUpUsername" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="signUpPassword">Password</Label>
-                    <Input id="signUpPassword" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter className="flex flex-col">
-                  <Button className="w-full">Sign Up</Button>
-                  <div className="flex m-4">
-                    <Separator />
-                    Sign Up with
-                    <Separator />
-                  </div>
-                  <Button variant="outline" className="w-12 h-12 rounded-full">
-                    <FaGoogle />
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="signup">
+            <Card className="shadow-2xl shadow-slate-900 bg-slate-900 bg-opacity-10">
+              <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                  Create a new account by providing the required information.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="signUpName">Name</Label>
+                  <Input id="signUpName" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="signUpEmail">Email</Label>
+                  <Input id="signUpEmail" type="email" />
+                </div>
+              </CardContent>
+              <CardFooter className="flex flex-col">
+                <Button className="w-full">Sign Up</Button>
+                <div className="w-full flex justify-center mt-6 mb-4">
+                  <Separator className="w-[30%]" />
+                  <span className="text-[10px] text-slate-500 w-fit mx-4 -mt-1 uppercase">or continue with</span>
+                  <Separator className="w-[30%]" />
+                </div>
+                <div className="flex justify-center space-x-4">
+                  <Button variant="outline" className="w-12 h-12 rounded-full p-0">
+                    <FaGoogle className="text-lg" />
                   </Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-          </Tabs>
-        </div>
+                  <Button variant="outline" className="w-12 h-12 rounded-full p-0">
+                    <FaGithub className="text-xl" />
+                  </Button>
+                </div>
+
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
+    </div>
   )
 }
 
